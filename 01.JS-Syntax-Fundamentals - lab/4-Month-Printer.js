44366ec83970829a74be2ad9750dbc32923ba9ad
Stela Hristova <stela.m.hristova@gmail.com>
2024-02-22 17:34:20 +0200

    first commit

---
+++ 01.JS-Syntax-Fundamentals - lab/4-Month-Printer.js
@@ -0,0 +1,31 @@
+function solve(data) {
+    if(data==1) {
+        console.log("January");
+    } else if(data==2) {
+        console.log("February");
+    } else if(data==3) {
+        console.log("March");
+    } else if(data==4) {
+        console.log("April");
+    } else if(data==5) {
+        console.log("May");
+    } else if(data==6) {
+        console.log("June");
+    } else if(data==7) {
+        console.log("July");
+    } else if(data==8) {
+        console.log("August");
+    } else if(data==9) {
+        console.log("September");
+    } else if(data==10) {
+        console.log("October");
+    } else if(data==11) {
+        console.log("November");
+    } else if(data==12) {
+        console.log("December");
+    } else {
+        console.log("Error!");
+    }
+}
+
+solve(13)
