7a14b232f20c567d2513b821e3032b10696dfdbf
Stela Hristova <stela.m.hristova@gmail.com>
2024-02-29 21:32:56 +0200

    added solution

---
+++ 04.JS Arrays and Strings – Lab/1.sum_first_and_last_array_elements.js
@@ -0,0 +1,19 @@
+//1
+function solve(numbers) {
+    let firstNumber = numbers.shift();
+    let lastNumber = numbers.pop();
+
+    console.log(firstNumber + lastNumber);
+}
+
+solve([20, 30, 40]);
+
+//2
+function solve(numbers) {
+    let firstNumber = numbers[0];
+    let lastNumber = numbers[numbers.length-1];
+
+    console.log(firstNumber + lastNumber);
+}
+
+solve([20, 30, 40]);
