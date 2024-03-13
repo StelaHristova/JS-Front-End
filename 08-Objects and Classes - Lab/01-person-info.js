4436756bcc801ae4542fcadc12d233ec4f59d202
Stela Hristova <stela.m.hristova@gmail.com>
2024-03-13 18:41:28 +0200

    added solution

---
+++ 08-Objects and Classes - Lab/01-person-info.js
@@ -0,0 +1,13 @@
+function solve(firstName, lastName, age) {
+    const personInfo = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    };
+
+    return personInfo;
+}
+
+solve("Peter", 
+"Pan",
+"20");
