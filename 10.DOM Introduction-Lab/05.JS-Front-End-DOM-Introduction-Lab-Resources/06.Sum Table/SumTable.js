9224c0613b7cdc1a9c10d070175a6871d1dd024f
Stela Hristova <stela.m.hristova@gmail.com>
2024-03-20 20:33:10 +0200

    added solution

--- 10.DOM Introduction-Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 10.DOM Introduction-Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,8 @@
 function sumTable() {
+    const tdCOstElements = document.querySelectorAll('tr td:last-of-type:not(#sum)');
+    const tdSumELement = document.getElementById('sum');
 
+    tdSumELement.textContent = Array
+        .from(tdCOstElements)
+        .reduce((sum, element) => sum + Number(element.textContent), 0);
 }
