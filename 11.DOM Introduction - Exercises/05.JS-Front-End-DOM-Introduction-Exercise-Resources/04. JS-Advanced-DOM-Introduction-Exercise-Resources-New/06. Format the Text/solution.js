48d02832d74584ad37363c031f138935e046b441
Stela Hristova <stela.m.hristova@gmail.com>
2024-03-23 21:06:27 +0200

    added solution

--- 11.DOM Introduction - Exercises/05.JS-Front-End-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 11.DOM Introduction - Exercises/05.JS-Front-End-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,9 @@
 function solve() {
-  //TODO
+  const textareaELement = document.getElementById('input');
+  const text = textareaELement.value;
+
+  //Convert to paragraphs
+
+  //Append to output element
+  
 }
