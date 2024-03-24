f3fb60ebe780cf283f1b1e78dad078d7ff884ee7
Stela Hristova <stela.m.hristova@gmail.com>
2024-03-24 20:19:24 +0200

    added solution

--- 11.DOM Introduction - Exercises/05.JS-Front-End-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 11.DOM Introduction - Exercises/05.JS-Front-End-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,9 +1,27 @@
 function solve() {
+  const outputElement = document.getElementById('output');
   const textareaELement = document.getElementById('input');
+
   const text = textareaELement.value;
 
   //Convert to paragraphs
+  const result = text
+    .split('.')
+    .filter(sentence => !!sentence)
+    .reduce((result, sentence, i) => {
+      const resultIndex = Math.floor(i / 3);
+      if (!result[resultIndex]) {
+        result[resultIndex] = []
+      }
+
+      result[resultIndex].push(sentence.trim());
+
+      return result;
+    }, [])
+    .map(sentences => `<p>${sentences.join('. ')}.</p>`)
+    .join('\n');
 
   //Append to output element
-  
-}
+    outputElement.innerHTML = result;
+} 
+
