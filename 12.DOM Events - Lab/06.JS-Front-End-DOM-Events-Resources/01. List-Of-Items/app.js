80ed79f0e2284ff41d8a482c25a9f1eb98b78acb
Stela Hristova <stela.m.hristova@gmail.com>
2024-03-26 19:11:04 +0200

    added solution

--- 12.DOM Events - Lab/06.JS-Front-End-DOM-Events-Resources/01. List-Of-Items/app.js
+++ 12.DOM Events - Lab/06.JS-Front-End-DOM-Events-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputElement = document.getElementById('newItemText');
+    const itemListELement = document.getElementById('items');
+
+    //Create new item
+    const newItemELement = document.createElement('li');
+
+    //Add text content
+    newItemELement.textContent = inputElement.value;
+
+    //Append new element to dom
+    itemListELement.appendChild(newItemELement);
+
+    //Clear input element
+    inputElement.value = '';
 }
--- 12.DOM Events - Lab/06.JS-Front-End-DOM-Events-Resources/04. Mouse-Gradient/app.js
+++ 12.DOM Events - Lab/06.JS-Front-End-DOM-Events-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientElement = document.getElementById('gradient');
+    const resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', (event) => {
+        const currentWIdth = event.offsetX;
+        const elementWidth = event.target.clientWidth;
+        const progress = Math.floor((currentWIdth / elementWidth) * 100)
+
+        resultElement.textContent = `${progress}%`;
+    });
 }
